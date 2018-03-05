b58dd0628cfeaa534ce34267cf82260f5b0a9c83
Sandhya <sandhyaj224@gmail.com>
2018-03-05 12:20:15 -0500

    First Commit

---
+++ src/main/java/com/example/web/MicroServicesDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesDemoApplication.class, args);
+	}
+}
